b7c42a786166174a854dbe2704398f90a4d6cb9b
sjones88 <steven.g.jones88@gmail.com>
2017-03-29 23:02:05 -0500

    test

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-$(() => {
+const blue = () => {
 	var $blue = $('div#blue')[0];
 	$blue.css({"background-color": "blue", "height": "100px", "width": "100px"});
 	$('.container').append($blue);
-});
+};
+
+$(blue());
